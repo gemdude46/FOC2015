eda61b5a9ed4adb57d5d646ab2fe8e2c9d7dc190
gemdude46 <gemdude46@gmail.com>
2015-07-28 14:00:13 +0100

    spaaaaaaaaaaaaaace

--- hyperspace/backend/main.py
+++ hyperspace/backend/main.py
@@ -1,19 +1,120 @@
 from flask import Flask
 from flask import request
+from flask import redirect
+import hashlib, os
 class MyServer(Flask):
 
     def __init__(self, *args, **kwargs):
             super(MyServer, self).__init__(*args, **kwargs)
 
             #instanciate your variables here
-            self.v = {}
+            self.v = {"users":[]}
+
+class User:
+    def __init__(self,user,pswd):
+        self.u=user
+        self.p=pswd
+    
+def newuser(u,p):
+    return User(u,hashlib.sha224(u+":"+p).hexdigest())
 
 app = MyServer(__name__)
 
 @app.route("/")
 def index():
-    return "This is the Index, why are you here?"
+    return """
+    
+    
+    <a href="register">Register</a><br>
+    <a href="login">Login</a>
+    
+    
+    
+    """
 
+@app.route("/register/")
+def register():
+    try:
+        u=request.args.get("usr")
+        p=request.args.get("psk")
+        if u==None or p==None :
+            raise KeyError
+        app.v["users"][u]=newuser(u,p)
+        return redirect("login")
+    except KeyError:
+        return """
+        
+        Register
+        
+        <form>
+        Username: <input type=text name=usr></input><br>
+        Password: <input type=text name=psk></input>
+        <input type=submit></input>
+        </form>
+        
+        
+        """
+        
+@app.route("/login/")
+def login():
+    return """
+        
+        Login
+        
+        <form action= method=POST>
+        Username: <input type=text name=usr></input><br>
+        Password: <input type=text name=psk></input>
+        <input type=submit></input>
+        </form>
+        
+        
+        """
+
+@app.route("/getsid/")
+def getsid():
+    sid=os.urandom(64)
+    return redirect("game?sid="+sid)
+
+@app.route("/game/")
+def game():
+    """
+    
+    
+    
+    <head><title>"""+request.args.get("usr")+"""
+<style>
+html, body {
+    width:  100%;
+    height: 100%;
+    margin: 0px;
+}
+</style>
+</head>
+<body>
+<canvas id=c>
+</canvas>
+<script>
+
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+function draw() {
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.rect(0,0,window.innerWidth,window.innerHeight);
+    ctx.fillStyle="black";
+    ctx.fill();
+}
+
+setInterval(draw,50);
+</script>
+</body>
+    
+    
+    
+    
+    
+    """
 
 if __name__ == "__main__":
     app.run()

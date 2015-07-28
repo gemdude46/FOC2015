9c96a19d7fd6e6356739891e7a0e3b7da55b65ec
gemdude46 <gemdude46@gmail.com>
2015-07-28 12:22:09 +0100

    it does a thing

--- hyperspace/backend/main.py
+++ hyperspace/backend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "This is the Index, why are you here?"
 
-if __name__ == “__main__”:
+
+if __name__ == "__main__":
     app.run()
